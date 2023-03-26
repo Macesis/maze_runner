86c0fb4807f7e58d6a5918c0619584c1c328c07d
Macesis <lm3gcz@gmail.com>
2023-03-26 22:08:20 +0200

    Some repairs on empty path

---
+++ build/CMakeFiles/test_gener.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_gener.dir/test_gener.cpp.o"
+  "CMakeFiles/test_gener.dir/test_gener.cpp.o.d"
+  "test_gener"
+  "test_gener.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_gener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
